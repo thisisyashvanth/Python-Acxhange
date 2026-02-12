ad5c6659c60deab9b01f4463a7bf14d800b95a26
Yashvanth <yashvanth.vijayabalaji@acxhange.com>
2026-02-12 09:40:07 +0530

    Encode Decode Completed

--- arithmetictables.py
+++ arithmetictables.py
@@ -1,17 +1,58 @@
+# num = int(input("Enter a number: "))
+
+# print("\nAddition Table")
+# for i in range(1, 11):
+#     print(f"{num} + {i} = {num + i}")
+
+# print("\nMultiplication Table")
+# for i in range(1, 11):
+#     print(f"{num} * {i} = {num * i}")
+
+# print("\nSubtraction Table")
+# for i in range(1, 11):
+#     print(f"{num} - {i} = {num - i}")
+
+# print("\nDivision Table")
+# for i in range(1, 11):
+#     print(f"{num} / {i} = {num / i:.2f}")
+
+def multiplication(num, i=1):
+    if i > 10:
+        return
+    print(f"{num}*{i} = {num * i}")
+    multiplication(num, i+1)
+
+def division(num, i=1):
+    if i > 10:
+        return
+    print(f"{num}/{i} = {num / i:.2f}")
+    division(num, i+1)
+
+def addition(num, i=1):
+    if i > 10:
+        return
+    print(f"{num}+{i} = {num + i}")
+    addition(num, i+1)
+
+def subtraction(num, i=1):
+    if i > 10:
+        return
+    print(f"{num}-{i} = {num - i}")
+    subtraction(num, i+1)
+
 num = int(input("Enter a number: "))
+print("Multiplication Table")
+multiply = multiplication(num)
+print()
 
-print("\nAddition Table")
-for i in range(1, 11):
-    print(f"{num} + {i} = {num + i}")
+print("Division Table")
+divide = division(num)
+print()
 
-print("\nMultiplication Table")
-for i in range(1, 11):
-    print(f"{num} * {i} = {num * i}")
+print("Addition Table")
+add = addition(num)
+print()
 
-print("\nSubtraction Table")
-for i in range(1, 11):
-    print(f"{num} - {i} = {num - i}")
-
-print("\nDivision Table")
-for i in range(1, 11):
-    print(f"{num} / {i} = {num / i:.2f}")
+print("Subtraction Table")
+subtract = subtraction(num)
+print()
